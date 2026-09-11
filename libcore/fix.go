1937e49ce8a2156794d569b59a00d90f82ef251b
Quinlan Hoo <syfrpv80@icloud.com>
2026-09-12 02:54:26 +0800

    chore: gomobile 纳入 go.mod replace，fix.go 保留至 Go 1.26

--- libcore/fix.go
+++ libcore/fix.go
@@ -2,6 +2,8 @@ package libcore
 
 // https://github.com/golang/go/issues/46893
 // TODO: remove after `bulkBarrierPreWrite: unaligned arguments` fixed
+// Fixed in cmd/cgo for Go 1.26 (CL 692935 aligns the export argument
+// struct); keep this while the toolchain pin is on Go 1.25.
 
 type StringBox struct {
 	Value string
--- libcore/go.mod
+++ libcore/go.mod
@@ -94,6 +94,8 @@ require (
 
 replace github.com/matsuridayo/libneko => ./libneko
 
+replace golang.org/x/mobile => ./gomobile
+
 replace github.com/sagernet/sing-box => ./sing-box
 
 replace github.com/sagernet/gvisor => github.com/sagernet/gvisor v0.0.0-20260727.0-sing-box-mod.1
