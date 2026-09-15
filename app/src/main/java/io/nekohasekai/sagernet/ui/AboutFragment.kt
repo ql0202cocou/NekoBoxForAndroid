1f80c055f4dff4e4a242b3ea7e1c12cd5c961989
Quinlan Hoo <syfrpv80@icloud.com>
2026-09-15 16:18:42 +0800

    fix: 更正关于页仓库地址为当前仓库

--- app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
@@ -32,7 +32,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-private const val REPO_URL = "https://github.com/ql0202cocou/QuinMoForAndroid"
+private const val REPO_URL = "https://github.com/ql0202cocou/NekoBoxForAndroid"
 
 class AboutFragment : ToolbarFragment(R.layout.layout_about) {
 
