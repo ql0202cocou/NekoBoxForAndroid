00350009ede8d8d6fecb43d5a66d0e279833b757
Quinlan Hoo <syfrpv80@icloud.com>
2026-09-15 16:19:11 +0800

    chore: jitpack 仓库限定为实际使用的两个依赖组

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,6 +1,12 @@
 repositories {
     google()
     mavenCentral()
-    gradlePluginPortal()
-    maven(url = "https://jitpack.io")
+    // jitpack serves arbitrary com.github.* builds; restrict it to the groups
+    // this project actually resolves from there (add a group when adding one).
+    maven(url = "https://jitpack.io") {
+        content {
+            includeGroup("com.github.jenly1314")
+            includeGroup("com.github.daniel-stoneuk")
+        }
+    }
 }
