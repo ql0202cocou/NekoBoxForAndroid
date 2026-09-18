c9300748979112a343d47f8de7adb358ff06bd2f
Quinlan Hoo <syfrpv80@icloud.com>
2026-09-18 16:11:57 +0800

    build(deps): Gradle 9.7.1、AGP 9.4.0、Kotlin 插件 2.4.20
    校验元数据随版本切换用 --write-verification-metadata 重建补齐。

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     // Gradle Plugins
-    implementation("com.android.tools.build:gradle:9.1.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    implementation("com.android.tools.build:gradle:9.4.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
 }
